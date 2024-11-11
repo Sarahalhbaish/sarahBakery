77205b4289e6262a64e5d5bb0245d7a2c5f0f8d7
Sarahalhbaish <sarah21995@live.com>
2024-11-11 18:52:09 +0300

    React 3 Complete

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import storeimg from "./Bakery.webp";
 import products from './products';
 
 function App() {
+  const ProductList = products.map((product, index) => (
+    <div key={index} className="Product"> 
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <h6>{product.price}</h6>
+    </div>
+  ))
+
   return (
     <div className="App">
        <h1>Sarah's Bakery</h1>
         <h3>Soft and Sweet</h3>
         <img className='Storeimg' src={storeimg} alt='bakery'/>
-        <div className="ProductList">
-        {products.map((product, index) => (
-          <div key={index} className="Product"> 
-            <img src={product.image} alt={product.name} />
-            <h3>{product.name}</h3>
-            <h6>{product.price}</h6>
-          </div>
-        ))}
+        <div className="Product_List">
+        {ProductList}
       </div>
     </div>
   );
