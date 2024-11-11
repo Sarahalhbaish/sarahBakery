cd6886fb0997f3d53ab5599e5a950653c69aca62
Sarahalhbaish <sarah21995@live.com>
2024-11-11 15:24:10 +0300

    React 3 Complete

--- src/App.js
+++ src/App.js
@@ -9,18 +9,15 @@ function App() {
        <h1>Sarah's Bakery</h1>
         <h3>Soft and Sweet</h3>
         <img className='Storeimg' src={storeimg} alt='bakery'/>
-        <div className='Product'>
-          <div>
-          <img src={products[0].image} alt={products[0].name} />
-          <h3>{products[0].name}</h3>
-          <h6>{products[0].price}</h6>
+        <div className="ProductList">
+        {products.map((product, index) => (
+          <div key={index} className="Product"> 
+            <img src={product.image} alt={product.name} />
+            <h3>{product.name}</h3>
+            <h6>{product.price}</h6>
           </div>
-          <div>
-          <img src={products[1].image} alt={products[1].name} />
-          <h3>{products[1].name}</h3>
-          <h6>{products[1].price}</h6>
-          </div>
-        </div>
+        ))}
+      </div>
     </div>
   );
 }
