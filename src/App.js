c7e1895b0f258a1b75c551aaaf6f50633e002b5c
Sarahalhbaish <sarah21995@live.com>
2024-11-11 15:03:20 +0300

    Task complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 
 import './App.css';
-import storeimg from "./Bakery.webp"
+import storeimg from "./Bakery.webp";
+import products from './products';
+
 function App() {
   return (
     <div className="App">
        <h1>Sarah's Bakery</h1>
         <h3>Soft and Sweet</h3>
-        <img className='storeimg' src={storeimg} alt='bakery'/>
+        <img className='Storeimg' src={storeimg} alt='bakery'/>
+        <div className='Product'>
+          <div>
+          <img src={products[0].image} alt={products[0].name} />
+          <h3>{products[0].name}</h3>
+          <h6>{products[0].price}</h6>
+          </div>
+          <div>
+          <img src={products[1].image} alt={products[1].name} />
+          <h3>{products[1].name}</h3>
+          <h6>{products[1].price}</h6>
+          </div>
+        </div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,17 @@
+import cakeImg from './cake.jpg';
+import pastriesImg from './pastries.jpeg';
+
+const products = [
+    {
+        name: "cake",
+        price: "10 KD",
+        image: cakeImg
+    },
+    {
+        name: "pastries",
+        price: "5 KD",
+        image: pastriesImg
+    }
+];
+
+export default products;
